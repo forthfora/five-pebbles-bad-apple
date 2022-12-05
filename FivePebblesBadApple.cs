c20bc300d20a83f796dd653c1b3d0d658dd24b0d
forthbridge <79264743+forthbridge@users.noreply.github.com>
2022-12-05 16:31:16 +0000

    IT WORKS!!
    The video plays all the way through without breaking, flickering and at full speed!!

--- FivePebblesBadApple.cs
+++ FivePebblesBadApple.cs
@@ -10,6 +10,13 @@ using UnityEngine;
 
 namespace FivePebblesBadApple
 {
+    public static class EnumExt_FPBA
+    {
+        public static SoundID Bad_Apple;
+        public static SSOracleBehavior.Action Degeneracy_BadApple;
+    }
+
+
     [BepInPlugin("forthbridge.five_pebbles_bad_apple", "FivePebblesBadApple", "0.1.0")]
     public class FivePebblesBadApple : BaseUnityPlugin
     {
@@ -21,12 +28,14 @@ namespace FivePebblesBadApple
         public FivePebblesBadApple() => self = new WeakReference(this);
         #endregion
 
-        public static List<string> frames = new List<string>();
+        public static Dictionary<string, byte[]> frames = new Dictionary<string, byte[]>();
+        public static AudioClip audio = new AudioClip();
 
         // The application of all hooks is delegated to a static class
         public void OnEnable() => Hooks.ApplyHooks();
 
         const string FRAMES_RESOURCE_PATH = "FivePebblesBadApple.Frames";
+        const string AUDIO_RESOURCE_PATH = "FivePebblesBadApple.BadApple.mp3";
 
         // Credit to LB on the RW Discord!
         // Loads all frames into the 'frames' array, each frame is an embedded resource png located under the 'Frames' directory in the assembly
@@ -34,18 +43,12 @@ namespace FivePebblesBadApple
         {
             // Get all embedded resources from the assembly via reflection
             Assembly assembly = Assembly.GetExecutingAssembly();
-
-            // Declare a Unity Texture2D
-            Texture2D frameTexture = new Texture2D(0, 0, TextureFormat.RGB24, false)
-            {
-                anisoLevel = 1,
-                filterMode = 0
-            };
+            int index = 0;
 
             foreach (string resourceName in assembly.GetManifestResourceNames())
             {
                 // Ensure that the resource is under the frames directory
-                if (!resourceName.StartsWith(FRAMES_RESOURCE_PATH)) continue;
+                if (!resourceName.StartsWith(FRAMES_RESOURCE_PATH) && resourceName != AUDIO_RESOURCE_PATH) return;
 
                 // Get both the resource stream from the resource name and declare a new memory stream
                 using Stream resourceStream = assembly.GetManifestResourceStream(resourceName);
@@ -57,20 +60,50 @@ namespace FivePebblesBadApple
                 // Write the resource stream into a memory stream
                 while ((read = resourceStream!.Read(buffer, 0, buffer.Length)) > 0) memoryStream.Write(buffer, 0, read);
 
-                // Write the current memory stream onto the texture
-                frameTexture.LoadImage(memoryStream.ToArray());
+                if (resourceName == AUDIO_RESOURCE_PATH)
+                {
+                    // Thanks to williamsenzo on the Unity Forums!
 
-                string frameName = "FPBadApple_" + FAtlasManager._nextAtlasIndex;
+                    // The size of a float is 4 bytes
+                    float[] samples = new float[memoryStream.ToArray().Length / 4];
+                    Buffer.BlockCopy(memoryStream.ToArray(), 0, samples, 0, memoryStream.ToArray().Length);
 
-                FAtlas atlas = new FAtlas(frameName, frameTexture, FAtlasManager._nextAtlasIndex);
-                Futile.atlasManager.AddAtlas(atlas);
-                FAtlasManager._nextAtlasIndex++;
+                    int channels = 1; // Mono Audio
+                    int sampleRate = 44100; // Standard CD Sample Rate
+
+                    audio = AudioClip.Create("BadApple", samples.Length, channels, sampleRate, false, false);
+                    audio.SetData(samples, 0);
+                    return;
+                }
+
+                string frameName = "FPBadApple_" + index;
 
                 // Finally, add the texture to the list of frames
-                frames.Add(frameName);
+                frames[frameName] = memoryStream.ToArray();
+                index++;
 
-                SELF.Logger_p.LogInfo(resourceName);
-                if (FAtlasManager._nextAtlasIndex > 500) break;
+                SELF.Logger_p.LogInfo("Loaded Frame " + resourceName + " as " + frameName);
+
+                // Only load the first 500 frames to speed up loading when debugging
+                // if (index > 500) break;
+            }
+        }
+    }
+
+    public class ProjectedImageFromMemory : ProjectedImage
+    {
+        public ProjectedImageFromMemory(List<Texture2D> textures, List<string> imageNames, int cycleTime) : base(imageNames, cycleTime)
+        {
+            if (textures == null || imageNames == null || textures.Count != imageNames.Count) return;
+
+            for (int i = 0; i < textures.Count; i++)
+            {
+                if (Futile.atlasManager.GetAtlasWithName(imageNames[i]) != null) break;
+
+                textures[i].wrapMode = TextureWrapMode.Clamp;
+                textures[i].anisoLevel = 0;
+                textures[i].filterMode = FilterMode.Point;
+                Futile.atlasManager.LoadAtlasFromTexture(imageNames[i], textures[i]);
             }
         }
     }
--- Hooks.cs
+++ Hooks.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -13,6 +14,11 @@ namespace FivePebblesBadApple
             // Rain World Startup
             On.RainWorld.Start += RainWorldStartHook;
 
+            On.SoundLoader.LoadSounds += SoundLoaderLoadSoundsHook;
+
+            //ProjectedImage constructor hook for hiding LoadFile()
+            On.ProjectedImage.ctor += ProjectedImageCtorHook;
+
             // Five Pebbles Update
             On.SSOracleBehavior.Update += SSOracleBehaviorUpdateHook;
         }
@@ -25,34 +31,38 @@ namespace FivePebblesBadApple
             FivePebblesBadApple.SELF.Logger_p.LogInfo("Finished Loading Frames!");
         }
 
-        //private static ProjectedImage image;
-        //private static bool isVideoPlaying = false;
+        private static void SoundLoaderLoadSoundsHook(On.SoundLoader.orig_LoadSounds orig, SoundLoader self)
+        {
+            orig(self);
 
-        //private static void SSOracleBehaviorUpdateHook(On.SSOracleBehavior.orig_Update orig, SSOracleBehavior self, bool eu)
-        //{
-        //    orig(self, eu);
+            // self.soundTriggers[(int)EnumExt_FPBA.Bad_Apple] = new SoundLoader.SoundTrigger(EnumExt_FPBA.Bad_Apple, null, 1.0f, self, new string[] { });
+        }
 
-        //    if (isVideoPlaying)
-        //    {
-        //        frameTimer += Time.deltaTime;
+        // ProjectedImage constructor hook for hiding LoadFile() (function cannot be overridden or hidden for ProjectedImage class)
+        static void ProjectedImageCtorHook(On.ProjectedImage.orig_ctor orig, ProjectedImage self, List<string> imageNames, int cycleTime)
+        {
+            //remove LoadFile() call from constructor, so no .PNG file is required
+            if (self is ProjectedImageFromMemory)
+            {
+                self.imageNames = imageNames;
+                self.cycleTime = cycleTime;
+                self.setAlpha = new float?(1f);
+                return;
+            }
+            orig(self, imageNames, cycleTime);
+        }
 
-        //        if (frameTimer <= 1.0f / FRAME_RATE) return;
-        //        frameTimer = 0.0f;
+        // How many frames are displayed each second
+        const int FRAME_RATE = 30;
 
-        //        image.Update(eu);
-        //        return;
-        //    }
-        //    isVideoPlaying = true;
+        // How many frames should we wait until the projected image is destroyed?
+        // This is necessary to prevent the projected image flickering, and I have zero clue why!
+        // Just know that 3 frames delay is the minimum to avoid flicker
+        const int IMAGE_DESTRUCTION_FRAME_DELAY = 3;
 
-        //    image = new ProjectedImage(FivePebblesBadApple.frames, 2);
-        //    image.setPos = new Vector2(midX, midY);
-
-        //    self.oracle.myScreen.images.Add(image);
-        //    self.oracle.myScreen.room.AddObject(image);
-
-        //}
-
-        const int FRAME_RATE = 5;
+        // We need to delay the destruction of a projected image's corresponding atlas, otherwise the game will freeze as the image will be missing its texture!
+        // We only need to wait a single frame, this ensures that the projected image is destroyed before the atlas
+        const int ATLAS_DESTRUCTION_FRAME_DELAY = 1;
 
         // Five Pebbles' Chamber Bounds
         const int minX = 200;
@@ -64,33 +74,66 @@ namespace FivePebblesBadApple
         public static int midY => minY + ((maxY - minY) / 2);
         public static int midX => minX + ((maxX - minX) / 2);
 
+
         // The index of the current frame, and a timer
         private static int currentFrame = 0;
-        private static float frameTimer = 0.0f;
+        private static float frameTimer;
 
-        // The image that was projected last frame
-        private static ProjectedImage lastImage;
+        private static Dictionary<int, ProjectedImage> projectedImageBuffer = new Dictionary<int, ProjectedImage>();
+
+        private static bool isVideoPlaying = false;
 
         private static void SSOracleBehaviorUpdateHook(On.SSOracleBehavior.orig_Update orig, SSOracleBehavior self, bool eu)
         {
             orig(self, eu);
 
-            frameTimer += Time.deltaTime;
+            if (!isVideoPlaying)
+            {
+                isVideoPlaying = true;
+                frameTimer = Time.time;
+                self.action = EnumExt_FPBA.Degeneracy_BadApple;
 
-            if (frameTimer <= 1.0f / FRAME_RATE) return;
-            frameTimer = 0.0f;
+                //self.oracle.room.PlaySound(EnumExt_FPBA.Bad_Apple, self.oracle.firstChunk);
+            }
 
-            ProjectedImage image = new ProjectedImage(new List<string> { FivePebblesBadApple.frames[currentFrame] }, 0);
-            image.setPos = new Vector2(midX, midY);
+            if (Time.time - frameTimer < 1.0f / FRAME_RATE) return;
+            frameTimer = Time.time;
 
-            if (lastImage != null) self.oracle.myScreen.room.RemoveObject(lastImage);
+            string frameName = FivePebblesBadApple.frames.ElementAt(currentFrame).Key;
+            byte[] textureBytes = FivePebblesBadApple.frames.ElementAt(currentFrame).Value;
 
-            self.oracle.myScreen.room.AddObject(image);
+            // Declare a Unity Texture2D and write the current byte array to the texture
+            Texture2D frameTexture = new Texture2D(0, 0, TextureFormat.RGB24, true);
+            frameTexture.LoadImage(textureBytes);
 
-            lastImage = image;
+            // Using the atlas we can create a projected image, which is what is used to display images in Pebbles' chamber
+            ProjectedImage projectedImage = new ProjectedImageFromMemory(new List<Texture2D> { frameTexture }, new List<string> { frameName }, 0);
+
+            // Add the image to the screen and set its position correctly (the center of the chamber)
+            self.oracle.myScreen.room.AddObject(projectedImage);
+
+            // There is also this offset to 'match background', but I am not sure why it is needed: new Vector2(-7.5f, 15.5f);
+            projectedImage.pos = new Vector2(midX, midY);
+
+
+            if (currentFrame - IMAGE_DESTRUCTION_FRAME_DELAY >= 0)
+            {
+                int targetFrame = currentFrame - IMAGE_DESTRUCTION_FRAME_DELAY;
+                self.oracle.myScreen.room.RemoveObject(projectedImageBuffer[targetFrame]);
+                FivePebblesBadApple.SELF.Logger_p.LogInfo("Removed Image: " + projectedImageBuffer[targetFrame]);
+            }
+
+            if (currentFrame - IMAGE_DESTRUCTION_FRAME_DELAY - ATLAS_DESTRUCTION_FRAME_DELAY >= 0)
+            {
+                int targetFrame = currentFrame - IMAGE_DESTRUCTION_FRAME_DELAY - ATLAS_DESTRUCTION_FRAME_DELAY;
+                Futile.atlasManager.ActuallyUnloadAtlasOrImage(FivePebblesBadApple.frames.ElementAt(targetFrame).Key);
+                FivePebblesBadApple.SELF.Logger_p.LogInfo("Removed Atlas: " + FivePebblesBadApple.frames.ElementAt(targetFrame).Key);
+            }
+
+            projectedImageBuffer[currentFrame] = projectedImage;
             currentFrame++;
 
-            FivePebblesBadApple.SELF.Logger_p.LogInfo(FivePebblesBadApple.frames[currentFrame]);
+            FivePebblesBadApple.SELF.Logger_p.LogInfo("Displaying Frame: " + frameName);
         }
     }
 }
