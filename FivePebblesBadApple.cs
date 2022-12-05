5c34e9324ff47fb05feb5d95e59b681e840e1ec9
forthbridge <79264743+forthbridge@users.noreply.github.com>
2022-12-05 11:10:30 +0000

    First attempt!
    It loads an image but it just flickers

--- FivePebblesBadApple.cs
+++ FivePebblesBadApple.cs
@@ -1,10 +1,77 @@
 ﻿using BepInEx;
+using BepInEx.Logging;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text.RegularExpressions;
+using UnityEngine;
 
-namespace RavioliWorld
+namespace FivePebblesBadApple
 {
-    [BepInPlugin("forthbridge.ravioli_world", "RavioliWorld", "0.1.0")]
-    public class RavioliWorld : BaseUnityPlugin
+    [BepInPlugin("forthbridge.five_pebbles_bad_apple", "FivePebblesBadApple", "0.1.0")]
+    public class FivePebblesBadApple : BaseUnityPlugin
     {
+        #region BepInEx Logger
+        // Allows access to BepInEx logger from other classes, see: https://rainworldmodding.miraheze.org/wiki/Code_Environments
+        private static WeakReference self;
+        public static FivePebblesBadApple SELF => self?.Target as FivePebblesBadApple;
+        public ManualLogSource Logger_p => Logger;
+        public FivePebblesBadApple() => self = new WeakReference(this);
+        #endregion
 
+        public static List<string> frames = new List<string>();
+
+        // The application of all hooks is delegated to a static class
+        public void OnEnable() => Hooks.ApplyHooks();
+
+        const string FRAMES_RESOURCE_PATH = "FivePebblesBadApple.Frames";
+
+        // Credit to LB on the RW Discord!
+        // Loads all frames into the 'frames' array, each frame is an embedded resource png located under the 'Frames' directory in the assembly
+        public static void LoadFrames()
+        {
+            // Get all embedded resources from the assembly via reflection
+            Assembly assembly = Assembly.GetExecutingAssembly();
+
+            // Declare a Unity Texture2D
+            Texture2D frameTexture = new Texture2D(0, 0, TextureFormat.RGB24, false)
+            {
+                anisoLevel = 1,
+                filterMode = 0
+            };
+
+            foreach (string resourceName in assembly.GetManifestResourceNames())
+            {
+                // Ensure that the resource is under the frames directory
+                if (!resourceName.StartsWith(FRAMES_RESOURCE_PATH)) continue;
+
+                // Get both the resource stream from the resource name and declare a new memory stream
+                using Stream resourceStream = assembly.GetManifestResourceStream(resourceName);
+                using MemoryStream memoryStream = new MemoryStream();
+
+                byte[] buffer = new byte[16384];
+                int read;
+
+                // Write the resource stream into a memory stream
+                while ((read = resourceStream!.Read(buffer, 0, buffer.Length)) > 0) memoryStream.Write(buffer, 0, read);
+
+                // Write the current memory stream onto the texture
+                frameTexture.LoadImage(memoryStream.ToArray());
+
+                string frameName = "FPBadApple_" + FAtlasManager._nextAtlasIndex;
+
+                FAtlas atlas = new FAtlas(frameName, frameTexture, FAtlasManager._nextAtlasIndex);
+                Futile.atlasManager.AddAtlas(atlas);
+                FAtlasManager._nextAtlasIndex++;
+
+                // Finally, add the texture to the list of frames
+                frames.Add(frameName);
+
+                SELF.Logger_p.LogInfo(resourceName);
+                if (FAtlasManager._nextAtlasIndex > 500) break;
+            }
+        }
     }
 }
---
+++ Hooks.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace FivePebblesBadApple
+{
+    static class Hooks
+    {
+        public static void ApplyHooks()
+        {
+            // Rain World Startup
+            On.RainWorld.Start += RainWorldStartHook;
+
+            // Five Pebbles Update
+            On.SSOracleBehavior.Update += SSOracleBehaviorUpdateHook;
+        }
+
+        private static void RainWorldStartHook(On.RainWorld.orig_Start orig, RainWorld self)
+        {
+            orig(self);
+            FivePebblesBadApple.SELF.Logger_p.LogInfo("Loading Frames...");
+            FivePebblesBadApple.LoadFrames();
+            FivePebblesBadApple.SELF.Logger_p.LogInfo("Finished Loading Frames!");
+        }
+
+        //private static ProjectedImage image;
+        //private static bool isVideoPlaying = false;
+
+        //private static void SSOracleBehaviorUpdateHook(On.SSOracleBehavior.orig_Update orig, SSOracleBehavior self, bool eu)
+        //{
+        //    orig(self, eu);
+
+        //    if (isVideoPlaying)
+        //    {
+        //        frameTimer += Time.deltaTime;
+
+        //        if (frameTimer <= 1.0f / FRAME_RATE) return;
+        //        frameTimer = 0.0f;
+
+        //        image.Update(eu);
+        //        return;
+        //    }
+        //    isVideoPlaying = true;
+
+        //    image = new ProjectedImage(FivePebblesBadApple.frames, 2);
+        //    image.setPos = new Vector2(midX, midY);
+
+        //    self.oracle.myScreen.images.Add(image);
+        //    self.oracle.myScreen.room.AddObject(image);
+
+        //}
+
+        const int FRAME_RATE = 5;
+
+        // Five Pebbles' Chamber Bounds
+        const int minX = 200;
+        const int maxX = 780;
+        const int minY = 60;
+        const int maxY = 640;
+
+        // Convenient methods for getting the center of the chamber
+        public static int midY => minY + ((maxY - minY) / 2);
+        public static int midX => minX + ((maxX - minX) / 2);
+
+        // The index of the current frame, and a timer
+        private static int currentFrame = 0;
+        private static float frameTimer = 0.0f;
+
+        // The image that was projected last frame
+        private static ProjectedImage lastImage;
+
+        private static void SSOracleBehaviorUpdateHook(On.SSOracleBehavior.orig_Update orig, SSOracleBehavior self, bool eu)
+        {
+            orig(self, eu);
+
+            frameTimer += Time.deltaTime;
+
+            if (frameTimer <= 1.0f / FRAME_RATE) return;
+            frameTimer = 0.0f;
+
+            ProjectedImage image = new ProjectedImage(new List<string> { FivePebblesBadApple.frames[currentFrame] }, 0);
+            image.setPos = new Vector2(midX, midY);
+
+            if (lastImage != null) self.oracle.myScreen.room.RemoveObject(lastImage);
+
+            self.oracle.myScreen.room.AddObject(image);
+
+            lastImage = image;
+            currentFrame++;
+
+            FivePebblesBadApple.SELF.Logger_p.LogInfo(FivePebblesBadApple.frames[currentFrame]);
+        }
+    }
+}
